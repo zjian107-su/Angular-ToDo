6545c4e704dda166964ba0ba126f64eef76d8a9e
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-06-18 10:30:28 -0400

    ✨ update: setup todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-ToDo';
+
+  newDescription = '';
+
+  allItems: Item[] = [
+    { description: 'Buy groceries', done: false },
+    { description: 'Cook dinner', done: false },
+    { description: 'Wash the dishes', done: false },
+    { description: 'Do laundry', done: false },
+    { description: 'Walk the dog', done: false },
+    { description: 'Take out the trash', done: false },
+    { description: 'Mow the lawn', done: false },
+  ];
+
+  addItem(item: Item) {
+    this.allItems.unshift(item);
+  }
+
+  worked() {
+    window.alert('Worked!');
+  }
 }
