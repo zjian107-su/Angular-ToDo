fafffadd42339dada0001f348a9057c9d573e14d
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-06-19 17:08:35 -0400

    ✨ update: setup todo-itemcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodoItemComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
