c4b9a9e12969a0515d5c2da0ded98485dc5f4d9b
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-06-19 11:48:37 -0400

    ✨ update: implement CRUD operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,12 @@ import { Item } from './item';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular-ToDo';
+  title: string = 'Angular-ToDo';
+  inEdit: boolean = false;
+  editIndex: number = -1;
 
   newDescription = '';
+  editDescription = 'Apple';
 
   allItems: Item[] = [
     { description: 'Buy groceries', done: false },
@@ -21,8 +24,28 @@ export class AppComponent {
     { description: 'Mow the lawn', done: false },
   ];
 
+  // CRUD - Create, Read, Update, Delete
   addItem(description: string) {
     this.allItems.unshift({ description, done: false });
     window.alert('It worked');
+  }
+
+  getItems() {
+    return this.allItems;
+  }
+
+  startEdit(index: number) {
+    this.inEdit = true;
+    this.editIndex = index;
+  }
+
+  editItem(newDescription: string) {
+    this.allItems[this.editIndex].description = this.editDescription;
+    this.inEdit = false;
+    this.editIndex = -1;
+  }
+
+  deleteItem(index: number) {
+    this.allItems.splice(index, 1);
   }
 }
