b6b4ef41fdcb407e898e91f85e2e00cc7c643825
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-06-20 14:43:22 -0400

    ✨ update: reformat add form, and add route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: AppComponent }];
 
 @NgModule({
   declarations: [AppComponent, TodoItemComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule {}
