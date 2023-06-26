48cf9f714024331f148fbc36946ff889877cf55e
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-06-26 11:31:56 -0400

    ✨ update: add template-driven form for user registration

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { User } from '../../interfaces/user';
+import { NgForm } from '@angular/forms';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-user-form',
@@ -7,18 +9,35 @@ import { User } from '../../interfaces/user';
   styleUrls: ['./user-form.component.css'],
 })
 export class UserFormComponent {
+  @ViewChild('f') signupForm!: any;
   userList: User[] = [];
 
   ngOnInit() {
     this.userList = [
-      { id: '1', name: 'Daniel' },
-      { id: '2', name: 'Mike' },
-      { id: '3', name: 'Jason' },
+      { id: '1', name: 'Daniel', age: '16' },
+      { id: '2', name: 'Mike', age: '17' },
+      { id: '3', name: 'Jason', age: '28' },
     ];
   }
 
-  register(name: string) {
-    this.userList.push({ id: (this.userList.length + 1).toString(), name });
-    console.log('Form submitted!');
+  onSubmitted(form: NgForm) {
+    console.log(form);
+    this.register(form.form.value.name, form.form.value.age);
+    this.signupForm.reset();
+  }
+
+  register(name: string, age: number) {
+    this.userList.push({
+      id: uuid(),
+      name,
+      age: age.toString(),
+    });
+  }
+
+  onAutoFill() {
+    this.signupForm.form.patchValue({
+      name: 'Robyn',
+      age: Math.floor(Math.random() * 50).toString(),
+    });
   }
 }
